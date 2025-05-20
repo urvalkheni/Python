e8adad1eb788314a88862ce453a4c83253155a49
Urval Kheni <kheniurval777@gmail.com>
2025-05-20 12:12:27 +0530

    Basic Started

---
+++ Basic/comment.py
@@ -0,0 +1,15 @@
+'''
+This is a comment line and here we use # for comment and \n for print in next line
+This Code is wrriten By Urval
+This is The Starting of python
+\n is a esc sequence charecter
+\" is use to print "
+\' is use to print '
+'''
+print("I am a \"Good Boy\" \nand You are also a good boy/girl")
+print("Hey",67,"So this is pyhton",sep='-',end="009\n")
+print("Hello")
+# this is a "Single-Line Comment"
+"""
+this is multi-line comment
+"""
