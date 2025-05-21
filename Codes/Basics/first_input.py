492d10d54dffdd00db8e41d41dbd9f8ca3f32441
Urval Kheni <kheniurval777@gmail.com>
2025-05-21 22:19:55 +0530

    Final Commit

---
+++ Codes/Basics/first_input.py
@@ -0,0 +1,8 @@
+# in python we can take input by use input
+a = input()
+print("My Number is ::",a)
+# defualt everthing is come as string in this so we have to convert it 
+# in desird data type
+x = int(input("Enter first Number :: "))
+y = int(input("Enter second Number :: "))
+print(x+y)
